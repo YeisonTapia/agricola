164abca80305ff0b0930663dbdf23c5e2c19045c
YeisonTapiaLozano <YeisonTapiaLozano>
2017-06-07 22:29:32 -0500

    Correccion bug de eliminacion

---
+++ resources/views/client/erroreliminated.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.principal')
+
+
+@section('content')
+<h4>
+<p>Error, No es posible eliminar debido a que este cliente tiene cuentas de cobro asociadas, si esta completamente seguro de eliminarlo por favor elimine manualmente las cuentas de cobro</p>
+</h4>
+@endsection
